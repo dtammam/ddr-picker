b5c6da4dfd866475cdc2fea72920bd35a84aa522
Dean <dean@tamm.am>
2024-05-17 21:02:32 -0400

    Refactored code to reference new module and standardized formatting

--- scripts/RestartComputer.ps1
+++ scripts/RestartComputer.ps1
@@ -1,39 +1,28 @@
 <#
-RestartComputer.ps1
-
-    Goal:
-        The purpose of this script is to restart your computer.
-
-    Audience:
-        People who want to be able to restart their computer.
-
-    Version:
-        8/27/2022 - Original version
-        9/2/2022 - Formatted text
-
-    Return Codes:
-        Success - 0
-        Failure - 1
-
-    References:
-        N/A
+.SYNOPSIS
+    Restarts the computer.
+.NOTES
+    Restart in a programmatic and clean way.
 #>
 
-# Define good/bad exit codes
-$SuccessExitCode = 0
-$FailureExitCode = 1
-$Host.UI.RawUI.WindowTitle = "Restart Computer"
+# Import core modules relevant for all scripts
+[string]$coreFunctionsModule = "$PSScriptRoot\CoreFunctions.psm1"
+Import-Module -Name $coreFunctionsModule -Force
 
-# Overarching Try block for execution
-Try {
+# Variable declaration
+$Host.UI.RawUI.WindowTitle = $scriptName
+
+try {
+    Open-Header
+
     Write-Output "Cabinet: Restarting Computer in 1 second..."
     Start-Sleep -Seconds 1
     Restart-Computer
-    Exit $SuccessExitCode
-}
 
-# Overarching Catch block for issues
-Catch {
-    Write-Output "Script failed with the following exception: $($_)"
-    Exit $FailureExitCode
+    $Script:exitCode = 0
+} catch {
+    Write-Output "Script failed with the following exception: [$($_.Message)]"
+    $Script:exitCode = 1
+} finally {
+    exit $Script:exitCode
 }
