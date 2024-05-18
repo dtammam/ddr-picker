977743fa1564850e3fde004708894d8feed00d30
Dean <dean@tamm.am>
2024-05-17 21:02:45 -0400

    Refactored code to reference new module and standardized formatting

--- scripts/StartLitForMAME.ps1
+++ scripts/StartLitForMAME.ps1
@@ -1,43 +1,32 @@
 <#
-StartLitForMAME.ps1
-
-    Goal:
-        The purpose of this script is to launch mame2lit.exe in an unobtrusive way.
-        mame2lit.exe is required for Litboard support with MAME games. It must be opened.
-
-    Audience:
-        People who use a frontend launcher and need mame2lit.exe to launch in an unobtrusive way.
-
-    Version:
-        9/3/2022
-
-    Return Codes:
-        Success - 0
-        Failure - 1
-
-    References:
-        Starting hidden processes - https://devblogs.microsoft.com/scripting/powertip-start-hidden-process-with-powershell/
+.SYNOPSIS
+    Start lights for MAME.
+.NOTES
+    Open the required executable silently which facilitates lights within MAME games.
+.LINK
+    Starting hidden processes - https://devblogs.microsoft.com/scripting/powertip-start-hidden-process-with-powershell/
 #>
 
-# Define good/bad exit codes and other relevant variables
-$SuccessExitCode = 0
-$FailureExitCode = 1
-$Host.UI.RawUI.WindowTitle = "Start Lit For MAME"
+# Import core modules relevant for all scripts
+[string]$coreFunctionsModule = "$PSScriptRoot\CoreFunctions.psm1"
+Import-Module -Name $coreFunctionsModule -Force
+
+# Variable declaration
+$Host.UI.RawUI.WindowTitle = $scriptName
 $ExePath = 'C:\pegasus\scripts\exe\mame2lit.exe'
 
-# Overarching Try block for execution
-Try {
+try {
+    Open-Header
+
     # Launch mame2lit.exe
     Write-Output "Cabinet: Launching mame2lit.exe..."
     Start-Process -WindowStyle Hidden -FilePath $ExePath
     Write-Output "Cabinet: launched mame2lit.exe."
 
-    # Clean exit
-    Exit $SuccessExitCode
-}
-
-# Overarching Catch block for issues
-Catch {
-    Write-Output "Script failed with the following exception: $($_)"
-    Exit $FailureExitCode
+    $Script:exitCode = 0
+} catch {
+    Write-Output "Script failed with the following exception: [$($_.Message)]"
+    $Script:exitCode = 1
+} finally {
+    exit $Script:exitCode
 }
