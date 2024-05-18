cc7c1b8fc48cf9a886f1ca5b35be9fa9fb78ab57
Dean <dean@tamm.am>
2024-05-17 21:02:12 -0400

    Refactored code to reference new module and standardized formatting

--- scripts/RegistryUpdatePCToKiosk.ps1
+++ scripts/RegistryUpdatePCToKiosk.ps1
@@ -1,49 +1,39 @@
 <#
-RegistryUpdatePCToKiosk.ps1
-
-    Goal:
-        The purpose of this script is to bring a computer out of PC mode and into kiosk mode.
-        This will allow someone to use their utility machine without the annoyance of explorer.exe.
-
-    Audience:
-        People who want to be able to optimize front-end usage of Windows-based utility machines.
-
-    Version:
-        8/27/2022 - Original version
-        9/2/2022 - Conversion from .bat/.reg to .ps1
-
-    Return Codes:
-        Success - 0
-        Failure - 1
-
-    References:
-        Modifying registry via Powershell - https://devblogs.microsoft.com/powershell-community/how-to-update-or-add-a-registry-key-value-with-powershell/
-        Microsoft documentation for New-ItemProperty - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-itemproperty?view=powershell-7.2
+.SYNOPSIS
+    Bring a computer out of PC mode into Kiosk mode.
+.NOTES
+    Replace shell to be a script that launches all utilities, as opposed to explorer.exe.
+.LINK
+    Modifying registry via Powershell - https://devblogs.microsoft.com/powershell-community/how-to-update-or-add-a-registry-key-value-with-powershell/
+.LINK
+    Microsoft documentation for New-ItemProperty - https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-itemproperty?view=powershell-7.2
 #>
 
-# Define good/bad exit codes
-$SuccessExitCode = 0
-$FailureExitCode = 1
-$Host.UI.RawUI.WindowTitle = "Registry Update PC to Kiosk"
+# Import core modules relevant for all scripts
+[string]$coreFunctionsModule = "$PSScriptRoot\CoreFunctions.psm1"
+Import-Module -Name $coreFunctionsModule -Force
 
-# Overarching Try block for execution
-Try {
+# Variable declaration
+$Host.UI.RawUI.WindowTitle = $scriptName
+
+try {
+    Open-Header
+
     Write-Output "Cabinet: Creating variables."
-    $RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-    $Name = 'Shell'
-	$Value = '"C:\pegasus\StartFrontendApps.exe"'
+    $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+    $name = 'Shell'
+	$value = '"C:\pegasus\StartFrontendApps.exe"'
     
-    Write-Output "Cabinet: Setting $($Name) to $($Value)..."
-    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType String -Force
-    Write-Output "Cabinet: Modified $($Name) to $($Value)."
+    Write-Output "Cabinet: Setting $($name) to $($value)..."
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force
+    Write-Output "Cabinet: Modified $($name) to $($value)."
 
     Write-Output "Cabinet: Restarting Computer now..."
     Restart-Computer
-    Exit $SuccessExitCode
-}
-
-# Overarching Catch block for issues
-Catch {
-    Write-Output "Script failed with the following exception: $($_)"
-    Exit $FailureExitCode
+    $script:exitCode = 0
+} catch {
+    Write-Output "Script failed with the following exception: [$($_.Message)]"
+    $script:exitCode = 1
+} finally {
+    exit $script:exitCode
 }
