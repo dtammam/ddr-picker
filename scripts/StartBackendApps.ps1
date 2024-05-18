8898e40ff1aaa15bdfe4993a5dae0410b1c5dae6
Dean <dean@tamm.am>
2024-05-17 21:02:41 -0400

    Refactored code to reference new module and standardized formatting

--- scripts/StartBackendApps.ps1
+++ scripts/StartBackendApps.ps1
@@ -1,41 +1,30 @@
 <#
-StartBackendApps.ps1
-
-    Goal:
-        The purpose of this script is to act as a 'Startup' for Windows apps that will no longer work without explorer.
-
-    Audience:
-        People who want to be able to 'normally' startup apps and processes without explorer.exe
-
-    Version:
-        8/27/2022 - Original version
-        9/2/2022 - Conversion from .bat to .ps1
-
-    Return Codes:
-        Success - 0
-        Failure - 1
-
-    References:
-        N/A
+.SYNOPSIS
+    Act as a 'Startup' for Windows apps that will no longer work without explorer.
+.NOTES
+    Critical for non-standard services that are required.
 #>
 
-# Define good/bad exit codes
-$SuccessExitCode = 0
-$FailureExitCode = 1
-$Host.UI.RawUI.WindowTitle = "Start Backend Apps"
+# Import core modules relevant for all scripts
+[string]$coreFunctionsModule = "$PSScriptRoot\CoreFunctions.psm1"
+Import-Module -Name $coreFunctionsModule -Force
 
-# Overarching Try block for execution
-Try {
+# Variable declaration
+$Host.UI.RawUI.WindowTitle = $scriptName
+
+try {
+    Open-Header
+
     Write-Output "Starting all backend services..."
     Start-Process "C:\Program Files (x86)\AudioSwitch\AudioSwitch.exe"
     Start-Process "C:\Program Files (x86)\Common Files\Apple\Internet Services\iCloudPhotos.exe"
     Start-Process "C:\Program Files (x86)\Common Files\Apple\Internet Services\iCloudServices.exe"
     Write-Output "All backend services started successfully."
-    Exit $SuccessExitCode
-}
-
-# Overarching Catch block for issues
-Catch {
-    Write-Output "Script failed with the following exception: $($_)"
-    Exit $FailureExitCode
+    
+    $Script:exitCode = 0
+} catch {
+    Write-Output "Script failed with the following exception: [$($_.Message)]"
+    $Script:exitCode = 1
+} finally {
+    exit $Script:exitCode
 }
