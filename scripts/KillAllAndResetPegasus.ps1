663c40ae30923e7f04e6a218c41b1532a40f3f8a
Dean Tammam <dean@tamm.am>
2024-06-15 23:38:22 -0400

    Array simplification
    Cleanup references to processes and window titles within arrays to reduce code reuse like a real programmer 🤔

--- scripts/KillAllAndResetPegasus.ps1
+++ scripts/KillAllAndResetPegasus.ps1
@@ -9,42 +9,51 @@
 [string]$coreFunctionsModule = "$PSScriptRoot\CoreFunctions.psm1"
 Import-Module -Name $coreFunctionsModule -Force
 
-# Variable declaration
-$Host.UI.RawUI.WindowTitle = $scriptName
-
 try {
     Open-Header
 
-    # Stop all relevant processes, with error actions set to silently continue in the event that some aren't open.
     Write-Log "Stopping all relevant processes..."
-    Stop-Process -Name pegasus-fe -ErrorAction SilentlyContinue
-    Stop-Process -Name spice -ErrorAction SilentlyContinue
-    Stop-Process -Name StepMania -ErrorAction SilentlyContinue
-    Stop-Process -Name mame -ErrorAction SilentlyContinue
-    Stop-Process -Name mame2lit -ErrorAction SilentlyContinue
-    Stop-Process -Name outfox -ErrorAction SilentlyContinue
-    Stop-Process -Name gslauncher -ErrorAction SilentlyContinue
-    Stop-Process -Name ITGMania -ErrorAction SilentlyContinue
-    Stop-Process -Name cmd -ErrorAction SilentlyContinue
-    Stop-Process -Name explorer -ErrorAction SilentlyContinue
-    Stop-Process -Name OpenITG-PC -ErrorAction SilentlyContinue
-    Stop-Process -Name OpenITG -ErrorAction SilentlyContinue
-    Stop-Process -Name Taskmgr -ErrorAction SilentlyContinue	
-    Stop-Process -Name cmd -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name timeout -ErrorAction SilentlyContinue	
-    Stop-Process -Name notepad -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name notepad++ -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name regedit -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name mmc -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name NotITG-v4.2.0 -ErrorAction SilentlyContinue -Force
-    Stop-Process -Name "In The Groove" -ErrorAction SilentlyContinue -Force
-    Get-Process | Where-Object { $_.MainWindowTitle -like '*RestartiCloudLoop' } | Stop-Process -ErrorAction SilentlyContinue -Force
-    Get-Process | Where-Object { $_.MainWindowTitle -like '*StartLitForMAME' } | Stop-Process -ErrorAction SilentlyContinue -Force
-    Get-Process | Where-Object { $_.MainWindowTitle -like '*StartMAME' } | Stop-Process -ErrorAction SilentlyContinue -Force
-    Get-Process | Where-Object { $_.MainWindowTitle -like '*ResetLitboardLights' } | Stop-Process -ErrorAction SilentlyContinue -Force
-    Get-Process | Where-Object { $_.MainWindowTitle -like '*StartBackendApps' } | Stop-Process -ErrorAction SilentlyContinue -Force
+    [array]$processesToKill = @(
+        'pegasus-fe'
+        'spice'
+        'StepMania'
+        'mame'
+        'mame2lit'
+        'outfox'
+        'gslauncher'
+        'ITGMania'
+        'cmd'
+        'explorer'
+        'OpenITG-PC'
+        'OpenITG'
+        'Taskmgr'
+        'cmd'
+        'timeout'
+        'notepad'
+        'notepad++'
+        'regedit'
+        'mmc'
+        'NotITG-v4.2.0'
+        'In The Groove'
+    )
 
-    # Start all relevant processes, ensuring that the frontend launcher starts last so it is the focused window.
+    foreach ($process in $processesToKill) { Stop-Process -Name $process -ErrorAction SilentlyContinue -Force }
+
+    Write-Log "Closing all relevant PowerShell windows..."
+    [array]$windowTitlesToKill = @(
+        'RestartiCloudLoop'
+        'StartLitForMAME'
+        'StartMAME'
+        'ResetLitboardLights'
+        'StartBackendApps'
+    )
+
+    foreach ($window in $windowTitlesToKill) {
+        Get-Process | 
+            Where-Object { $_.MainWindowTitle -like "*$window*" } | 
+            Stop-Process -ErrorAction SilentlyContinue -Force
+    }
+
     Write-Log "Starting all relevant processes..."
     Start-Process PowerShell C:\pegasus\scripts\ResetLitboardLights.ps1 -WindowStyle Hidden
     Start-Process PowerShell C:\Pegasus\scripts\RestartiCloudLoop.ps1 -WindowStyle Minimized
