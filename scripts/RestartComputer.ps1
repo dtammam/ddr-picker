99a94acdd06c543270660008a2c6bfbffb9c6a2a
Dean Tammam <dean@tamm.am>
2024-10-01 21:18:17 -0400

    So many awesome changes! Such a better UX!

--- scripts/RestartComputer.ps1
+++ scripts/RestartComputer.ps1
@@ -14,6 +14,7 @@ try {
 
     Write-Log "Restarting Computer in 1 second..."
     Start-Sleep -Seconds 1
+    Start-Sound ("C:\Games\Megatouch_PhotoHunt_Phew.wav")
     Restart-Computer
     $Script:exitCode = 0
 } catch {
